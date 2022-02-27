326ff50e1c80675cc5a807ea58da453fd2123a6f
Kal <info@kalacademy.org>
2022-02-27 11:06:32 -0800

    React bootstrap and bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import {Container} from 'react-bootstrap';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 //arrow functions
 const App = () => {
   return (
     <>
-      <h1>Welcome to MyShop!</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to MyShop!</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
