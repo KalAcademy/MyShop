98c62e85da1e6e15b8c434972064cf157132feae
Kal <info@kalacademy.org>
2022-03-06 10:57:51 -0800

    Phase 1 of frontend complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import {Container} from 'react-bootstrap';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 //arrow functions
 const App = () => {
   return (
     <>
-      <Header />
-      <main>
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen />} exact />
+              <Route path='/product/:id' element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   );
 }
