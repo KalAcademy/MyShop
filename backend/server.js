b8144cd3cae6f03255d1f9c2952e6efe42ae11d8
Kal <info@kalacademy.org>
2022-03-06 11:10:46 -0800

    backend project

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.listen(5000, console.log('Server is running on port 5000'));
