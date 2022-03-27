4cc423a984002595a25628e5aaa76a4cba8df49a
Kal <info@kalacademy.org>
2022-03-27 11:05:10 -0700

    Product list reducer and action

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -47,6 +47,11 @@ const productSchema = mongoose.Schema({
     required: true,
     default: 0
   },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
   price: {
     type: Number,
     required: true,
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,17 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import { productListReducer } from './reducers/productReducers'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer
+})
 const initialState = {}
 const middleware = [thunk]
 const store = createStore(
                 reducer, 
                 initialState,
-                applyMiddleware(...middleware)
+                composeWithDevTools(applyMiddleware(...middleware))
                 )
 
 export default store
