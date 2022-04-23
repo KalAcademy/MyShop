fbadf899d39b619bd41f816b9b18ede21695526f
Kal <info@kalacademy.org>
2022-04-23 10:52:51 -0700

    Prepared to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
@@ -9,10 +10,6 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 const app = express()
 dotenv.config()
 connectDB()
-
-app.get('/', (req, res) => {
-  res.json({'msg': 'Hello World'})
-})
 
 app.use(express.json())
 app.use('/api/products', productRoutes)
@@ -24,6 +21,14 @@ app.get('/api/config/paypal', (req, res) =>
 )
 app.use(notFound)
 app.use(errorHandler)
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('frontend/build'))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
