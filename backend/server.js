cd67a75025f2abe4919912bcf58ce81818cc670a
Kal <info@kalacademy.org>
2022-04-23 11:02:12 -0700

    Prepared to deploy

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,10 @@ app.get('/api/config/paypal', (req, res) =>
 app.use(notFound)
 app.use(errorHandler)
 
+const __dirname = path.resolve()
+
 if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('frontend/build'))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
