376df3991905f5a751c88bc11e0e0dd771436c6c
Kal <info@kalacademy.org>
2022-04-23 18:49:05 -0700

    Prepared to deploy

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,7 @@ app.use('/api/orders', orderRoutes)
 app.get('/api/config/paypal', (req, res) => 
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
-app.use(notFound)
-app.use(errorHandler)
+
 
 const __dirname = path.resolve()
 
@@ -30,7 +29,12 @@ if (process.env.NODE_ENV === 'production'){
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   })
+}else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
-
+app.use(notFound)
+app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
